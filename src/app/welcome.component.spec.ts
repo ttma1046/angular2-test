61277ca3265d39d734b20d4521f2b59c9c5f959a
Chandler Fang <chandlerfang@gmail.com>
2017-03-24 13:28:43 +1100

    Test a component with a dependency.

---
+++ src/app/welcome.component.spec.ts
@@ -0,0 +1,70 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By }              from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { WelcomeComponent } from './welcome.component';
+import { UserService } from './model';
+
+describe('WelcomeComponent', () => {
+
+  let comp: WelcomeComponent;
+  let fixture: ComponentFixture<WelcomeComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
+
+  let userServiceStub: any;
+  let userService: any;
+
+  // async beforeEach
+  beforeEach(() => {
+    // stub UserService for test purposes
+    userServiceStub = {
+      isLoggedIn: true,
+      user: { name: 'Test User' }
+    };
+
+    TestBed.configureTestingModule({
+      declarations: [WelcomeComponent],
+      providers: [{ provide: UserService, useValue: userServiceStub }]
+    });
+
+    fixture = TestBed.createComponent(WelcomeComponent);
+    comp = fixture.componentInstance;
+
+    // UserService from the root injector
+    userService = TestBed.get(UserService);
+
+    //  get the "welcome" element by CSS selector (e.g., by class name)
+    de = fixture.debugElement.query(By.css('.welcome'));
+    el = de.nativeElement;
+  });
+
+  it('stub object and injected UserService should not be the same', () => {
+    expect(userServiceStub === userService).toBe(false);
+
+    // Changing the stub object has no effect on the injected service
+    userServiceStub.isLoggedIn = false;
+    expect(userService.isLoggedIn).toBe(true);
+  });
+
+  it('should welcome the user', () => {
+    fixture.detectChanges();
+    const content = el.textContent;
+    expect(content).toContain('Welcome', '"Welcome ..."');
+    expect(content).toContain('Test User', 'expected name');
+  });
+
+  it('should welcome "Bubba"', () => {
+    userService.user.name = 'Bubba'; // welcome message hasn't been shown yet
+    fixture.detectChanges();
+    expect(el.textContent).toContain('Bubba');
+  });
+
+  it('should request login if not logged in', () => {
+    userService.isLoggedIn = false; // welcome message hasn't been shown yet
+    fixture.detectChanges();
+    const content = el.textContent;
+    expect(content).not.toContain('Welcome', 'not welcomed');
+    expect(content).toMatch(/log in/i, '"log in"');
+  });
+});
